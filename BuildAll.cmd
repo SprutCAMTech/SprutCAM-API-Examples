593345f0033a67a11f1313ec319151120b5bb67c
duromanov <duromanov@sprut.ru>
2022-03-28 12:40:52 +0300

    Added BuildAll cmd file, readme updated

---
+++ BuildAll.cmd
@@ -0,0 +1,24 @@
+@echo off
+SETLOCAL
+SET EXIT_CODE=0
+SET BUILDCONFIG=%1
+SET version=
+
+if "%BUILDCONFIG%" == "" (SET BUILDCONFIG=DEBUG)
+
+call :RUNCLEAN "STMachiningToolsImportApi\STConsoleApp.csproj"
+
+call :RUNBUILD "STMachiningToolsImportApi\STConsoleApp.csproj"
+
+echo Total result %EXIT_CODE%
+EXIT /B %EXIT_CODE%
+
+:RUNCLEAN
+    call dotnet clean --configuration %BUILDCONFIG% "%~1" --verbosity minimal
+    if NOT "%ERRORLEVEL%" == "0" (SET EXIT_CODE=1)
+EXIT /B
+
+:RUNBUILD
+    call dotnet build --configuration %BUILDCONFIG% "%~1" --verbosity minimal
+    if NOT "%ERRORLEVEL%" == "0" (SET EXIT_CODE=1)
+EXIT /B
--- README.md
+++ README.md
@@ -4,5 +4,7 @@ Examples of using API for [**SprutCAM**](https://www.sprutcam.com).
 To be possible to build and run these examples you should install SprutCAM before, because they depend on SprutCAM's libraries. 
 You can use download links from [ the official site](https://www.sprutcam.com).
 
-Documentation of SprutCAM's API can be found [here](https://kb.sprutcam.com/docs/Inp.Net/16/en/).
+Documentation of SprutCAM's API:
+- [Postprocessing subsystem](https://kb.sprutcam.com/docs/Inp.Net/16/en/)
+- [Machining tools import](https://kb.sprutcam.com/docs/Inp.Net/16/en/articles/machiningtoolsimport/MTIStartPage.html)
 
